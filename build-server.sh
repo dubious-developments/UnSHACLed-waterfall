79f0e589927cb7dda58f255f2a95b7035455c07c
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-22 19:00:37 +0100

    Create a version number during build

--- build-server.sh
+++ build-server.sh
@@ -5,6 +5,11 @@ current_dir_path=$(dirname $0)
 pushd $current_dir_path/UnSHACLed-server/UnSHACLed
 # Download dependencies.
 npm install
+
 # Build the client.
 gulp build
+
+# Create a version number file.
+echo "UnSHACLed build $1" > version
+
 popd
