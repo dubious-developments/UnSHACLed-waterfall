e6e82cfc3a103e9398d204260c013b68a8892beb
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-23 17:16:33 +0100

    Update server submodule

--- patch-server.sh
+++ patch-server.sh
@@ -4,7 +4,8 @@ current_dir_path=$(dirname $0)
 
 pushd $current_dir_path/UnSHACLed-server
 
-echo $1 | sudo --stdin cp sites-enabled-default /etc/nginx/sites-available/default
+./patch-server.sh $2 $3
+echo $1 | sudo --stdin ./install-server.sh
 echo $1 | sudo --stdin service nginx reload
 
 popd
