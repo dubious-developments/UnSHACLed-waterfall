34778b05677a472b18a0e141ca0609f56a72b9a3
jonathanvdc <jonathan.vdc@outlook.com>
2018-05-21 22:04:19 +0200

    Update draft-release.sh

--- draft-release.sh
+++ draft-release.sh
@@ -2,10 +2,19 @@
 set -e
 
 # Drafts a new release. Release branch is specified as first argument.
+
+# Figure out what the current branch is so we can go back
+# to our cosy little corner of the Universe when we're done here.
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Create a new release branch.
 RELEASE_BRANCH=$1
-git checkout -b $RELEASE_BRANCH
+git checkout -B $RELEASE_BRANCH
+
+# Update all submodules.
 git submodule update --recursive --init
 
+# Pull in latest versions of submodules. Update recursively.
 pushd UnSHACLed-server
 pushd UnSHACLed
 git pull origin master
@@ -19,5 +28,9 @@ pushd UnSHACLed-collaboration-server
 git pull origin master
 popd
 
+git add .
 git commit -m "Update UnSHACLed server to latest version"
 git push --set-upstream origin $RELEASE_BRANCH
+
+# Switch back to current branch.
+git checkout $CURRENT_BRANCH
